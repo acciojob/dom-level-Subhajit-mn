9444f333ea75f3a0e302cb53d9ea9a88031719d2
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-19 23:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const element = document.getElementById("level");
+let i=0;
+while(element = element.parentNode){
+	if(element.getAttribute('id') === "level"){
+		i++;
+	}
+}
+alert("The level of the element is: "+ i);
