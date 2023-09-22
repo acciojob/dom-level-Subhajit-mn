78d9214019f02ba438580b9df56bf780d8abf915
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-22 15:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-	const levleElement = document.getElementById("level");
+	const levelElement = document.getElementById("level");
 	let level=1;
 	while(true){
-		if(level.Element.tagName === "HTML"){
+		if(levelElement.tagName === "HTML"){
 			break;
 		}else{
 			levelElement = levelElement.parentNode;
