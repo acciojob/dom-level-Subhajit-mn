4b791d52a4e6687e32afcad762ff3b5371a57cb3
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-22 12:39:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //your JS code here. If required.
-const element = document.getElementById("level");
+const ele = document.getElementById("level");
 let i=0;
-while(element = element.parentNode){
-	if(element.getAttribute('id') === "level"){
+while(true){
+	if(level.Element.TagName === "HTML"){
+		break;
+	}else{
+		ele = ele.ParentNode;
 		i++;
 	}
 }
