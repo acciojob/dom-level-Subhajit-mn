8ab4429761c6dc53698f6225929946dbefe3996a
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-22 15:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-	const levelElement = document.getElementById("level");
+	let levelElement = document.getElementById("level");
 	let i=1;
 	while(true){
 		if(levelElement.tagName === "HTML"){
