4ba975f17c305d6aca97b64538d031d3d0fa8457
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-09-22 12:55:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //your JS code here. If required.
-const levleElement = document.getElementById("level");
-let level=1;
-while(true){
-	if(level.Element.tagName === "HTML"){
-		break;
-	}else{
-		levelElement = levelElement.parentNode;
-		level++;
+document.addEventListener("DOMcontentLoaded", () =>{
+	const levleElement = document.getElementById("level");
+	let level=1;
+	while(true){
+		if(level.Element.tagName === "HTML"){
+			break;
+		}else{
+			levelElement = levelElement.parentNode;
+			level++;
+		}
 	}
-}
-alert(`The level of the element is: ${level}`);
+	alert(`The level of the element is: ${level}`);
+});
